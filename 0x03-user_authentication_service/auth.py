30bb5466ca5aa4fdc1cedd51890b738377cbef48
Toluwaloju0 <teecoded1999@gmail.com>
2024-11-22 13:04:26 +0100

    user auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -28,15 +28,15 @@ class Auth:
             user = self._db.find_user_by(email=email)
             raise ValueError(f"User {user.email} already exists")
         except NoResultFound:
-            return self._db.add_user(email, password)
+            return self._db.add_user(email, _hash_password(password))
 
     def valid_login(self, email: str, password: str) -> bool:
         """ A method to check if the password for a user is correct"""
 
         try:
             user = self._db.find_user_by(email=email)
-            return checkpw(password.encode('utf-8'), user.hashed_password)
-        except (NoResultFound):
+            return checkpw(password.encode("utf-8"), user.hashed_password)
+        except NoResultFound:
             return False
 
     def _generate_uuid(self) -> str:
